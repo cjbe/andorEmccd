dd98416c741f751e1b562701e71a718bad5152dc
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2019-01-22 22:14:48 +0000

    Add support for driving multiple cameras
    This changes the nature of the background acquisition thread slightly. Instead
    of blocking on the library call, it polls every ~20ms. This should not change
    anything significantly

--- andorEmccd/andorEmccd.py
+++ andorEmccd/andorEmccd.py
@@ -5,8 +5,10 @@ import os
 import numpy as np
 import collections
 import threading
+import contextlib
 
 from ctypes import c_int, c_float, c_long, c_ulong, POINTER
+
 
 # Constants for SetTrigger()
 TRIGGER_INTERNAL = 0
@@ -34,10 +36,13 @@ class LibInstance:
         20069: "DRV_P4INVALID",
         20072: "DRV_ACQUIRING",
         20073: "DRV_IDLE",
-        20075: "DRV_NOT_INITIALIZED"
+        20075: "DRV_NOT_INITIALIZED",
+        20992: "DRV_NOT_AVAILABLE"
     }
 
     def __init__(self):
+        self.lock = threading.Lock()
+
         if platform.system() == "Windows":
             os.environ['PATH'] = "C:\Program Files\Andor SOLIS\Drivers" + ';' \
                                     + os.environ['PATH']
@@ -129,10 +134,48 @@ class LibInstance:
         self.get_images = wrapper(dll.GetImages,
             [c_long, c_long, POINTER(c_int), c_ulong, POINTER(c_long), POINTER(c_long)])
         self.shutdown = wrapper(dll.ShutDown)
-        self.get_available_cameras = wrapper(dll.GetAvailableCameras, [POINTER(c_int)])
-        self.get_camera_handle = wrapper(dll.GetCameraHandle, [c_int, POINTER(c_int)])
+        self.get_available_cameras = wrapper(dll.GetAvailableCameras, [POINTER(c_long)])
+        self.get_camera_handle = wrapper(dll.GetCameraHandle, [c_int, POINTER(c_long)])
         self.get_camera_serial = wrapper(dll.GetCameraSerialNumber, [POINTER(c_int)])
-        self.set_current_camera = wrapper(dll.SetCurrentCamera, [c_int])
+        self.set_current_camera = wrapper(dll.SetCurrentCamera, [c_long])
+
+    @contextlib.contextmanager
+    def lock_select_camera(self, camera_index):
+        """Provide sole access to the library, guaranteeing the given camera remains
+        selected during the 'with' block.
+
+        When working with a single camera the GIL means we don't have to be careful about
+        the multi-threading. However when several cameras are used other threads can
+        change the selected camera underneath us."""
+        self.lock.acquire()
+        self.set_current_camera(camera_index)
+        try:
+            yield
+        finally:
+            self.lock.release()
+
+
+def initialise_all_cameras():
+    """Initialises all connected cameras.
+    Returns a dictionary of AndorEmccd instances indexed by serial number
+    """
+    lib = LibInstance()
+
+    n_cameras = c_long()
+    lib.get_available_cameras(ctypes.byref(n_cameras))
+
+    cameras = {}
+    for i in range(n_cameras.value):
+        handle = c_long()
+        lib.get_camera_handle(i, ctypes.byref(handle))
+        with lib.lock_select_camera(handle):
+            # Initialize() needs to be called for every camera
+            lib.initialize()
+
+            serial = c_int()
+            lib.get_camera_serial(ctypes.byref(serial))
+        cameras[serial.value] = AndorEmccd(lib=lib, camera_handle=handle)
+    return cameras
 
 
 class AndorEmccd:
@@ -141,26 +184,37 @@ class AndorEmccd:
     is not acquiring"""
     dll = None
 
-    def __init__(self, leave_camera_warm=True, framebuffer_len=100):
+    def __init__(self, leave_camera_warm=True, framebuffer_len=100, lib=None,
+            camera_handle=None):
         """Initialise the camera interface.
 
         leave_camera_warm: turn off the cooler when disconnecting from the
         camera.
-
         framebuffer_len: maximum number of stored frames before oldest are discarded
+        lib: SDK library instance to use. If none, one will be instantiated
+        camera_handle: driver handle of camera to use. If None, assume a single camera
+            system.
         """
         self.leave_camera_warm = leave_camera_warm
 
-        self.lib = LibInstance()
-        self.lib.initialize()
+        if lib is None:
+            self.lib = LibInstance()
+            self.lib.initialize()
+        else:
+            self.lib = lib
 
-        self.lib.set_cooler_mode(not self.leave_camera_warm)
+        if camera_handle:
+            self.lock_camera = lambda: self.lib.lock_select_camera(camera_handle)
+        else:
+            # We use suppress() here as nocontext() does not exist before python 3.7
+            self.lock_camera = contextlib.suppress
 
-        # We are responsible adults - we can handle the full EM gain!
-        self.lib.set_em_advanced(1)
-        
-        # Set 'real EM gain' mode
-        self.lib.set_em_gain_mode(3)
+        with self.lock_camera():
+            self.lib.set_cooler_mode(not self.leave_camera_warm)
+            # We are responsible adults - we can handle the full EM gain!
+            self.lib.set_em_advanced(1)
+            # Set 'real EM gain' mode
+            self.lib.set_em_gain_mode(3)
 
         # Find the available gains, shift speeds, etc
         self._get_preamp_gains()
@@ -173,7 +227,8 @@ class AndorEmccd:
 
         horiz = c_int()
         vert = c_int()
-        self.lib.get_detector(ctypes.byref(horiz), ctypes.byref(vert))
+        with self.lock_camera():
+            self.lib.get_detector(ctypes.byref(horiz), ctypes.byref(vert))
         self.ccdWidth = horiz.value
         self.ccdHeight = vert.value
 
@@ -184,15 +239,18 @@ class AndorEmccd:
         # Sensible defaults
         self.set_shutter_open(True)
         self.set_trigger_mode(TRIGGER_INTERNAL)
-        self.lib.set_kinetic_cycle_time(0)
+        with self.lock_camera():
+            self.lib.set_kinetic_cycle_time(0)
 
         self.frame_buffer = collections.deque([], framebuffer_len)
 
         self._frame_call_list = []
 
+        self._stopping = threading.Event()
+
         # Start image acquisition thread
-        t = threading.Thread(target=self._acquisition_thread, daemon=True)
-        t.start()
+        self._thread = threading.Thread(target=self._acquisition_thread, daemon=True)
+        self._thread.start()
 
     def __del__(self):
         self.close()
@@ -201,40 +259,46 @@ class AndorEmccd:
         """Leave the camera in a safe state and shutdown the driver"""
         if self.lib is None:
             return
+        self._stopping.set()
         if self.leave_camera_warm:
             self.stop_acquisition()
             self.set_temperature(10)
-        self.lib.shutdown()
+        self._thread.join()
+        with self.lock_camera():
+            self.lib.shutdown()
         self.lib = None
 
     def _get_preamp_gains(self):
         tmp = c_int()
-        self.lib.get_number_preamp_gains(ctypes.byref(tmp))
-        n_gains = tmp.value
-        self.preamp_gains = []
-        for i in range(n_gains):
-            tmp = c_float()
-            self.lib.get_preamp_gain(i, ctypes.byref(tmp))
-            self.preamp_gains.append( round(tmp.value,1) )
+        with self.lock_camera():
+            self.lib.get_number_preamp_gains(ctypes.byref(tmp))
+            n_gains = tmp.value
+            self.preamp_gains = []
+            for i in range(n_gains):
+                tmp = c_float()
+                self.lib.get_preamp_gain(i, ctypes.byref(tmp))
+                self.preamp_gains.append( round(tmp.value,1) )
 
     def set_preamp_gain(self, gain):
         """Sets the pre-amp gain. gain must be one of the values in 
         self.preamp_gains"""
         try:
-            index = self.preamp_gains.index(gain)
+            index = self.preamp_gains.index(int(gain))
         except ValueError:
             raise ValueError("Pre-amp gain not in {}".format(self.preamp_gains))
-        self.lib.set_preamp_gain(index)
+        with self.lock_camera():
+            self.lib.set_preamp_gain(index)
 
     def _get_vertical_shift_speeds(self):
         tmp = c_int()
-        self.lib.get_number_vs_speeds(ctypes.byref(tmp))
-        n_gains = tmp.value
-        self.vertical_shift_speeds = []
-        for i in range(n_gains):
-            tmp = c_float()
-            self.lib.get_vs_speed(i, ctypes.byref(tmp))
-            self.vertical_shift_speeds.append( round(tmp.value,1) )
+        with self.lock_camera():
+            self.lib.get_number_vs_speeds(ctypes.byref(tmp))
+            n_gains = tmp.value
+            self.vertical_shift_speeds = []
+            for i in range(n_gains):
+                tmp = c_float()
+                self.lib.get_vs_speed(i, ctypes.byref(tmp))
+                self.vertical_shift_speeds.append( round(tmp.value,1) )
 
     def set_vertical_shift_speed(self, vs_speed):
         """Sets the vertical shift speed in us. vs_speed must be one of the 
@@ -244,42 +308,45 @@ class AndorEmccd:
         except ValueError:
             raise ValueError("Vertical shift speed not in {}".format(
                                             self.vertical_shift_speeds))
-        self.lib.set_vs_speed(index)
+        with self.lock_camera():
+            self.lib.set_vs_speed(index)
 
     def set_vertical_clock_voltage(self, boost):
         """Set the vertical shift clock voltage boost.
         The valid range is integers between 0 (normal) and 4 (max boost)"""
-        self.lib.set_vs_amplitude(boost)
+        with self.lock_camera():
+            self.lib.set_vs_amplitude(int(boost))
 
     def _get_horizontal_shift_speeds(self):
         # Get the number of ADC channels
-        tmp = c_int()
-        self.lib.get_number_adc_channels(ctypes.byref(tmp))
-        n_adcs = tmp.value
+        with self.lock_camera():
+            tmp = c_int()
+            self.lib.get_number_adc_channels(ctypes.byref(tmp))
+            n_adcs = tmp.value
 
-        # Get the bit depths of the ADCs
-        bit_depth = []
-        for i in range(n_adcs):
-            self.lib.get_bit_depth(i, ctypes.byref(tmp))
-            bit_depth.append(tmp.value)
+            # Get the bit depths of the ADCs
+            bit_depth = []
+            for i in range(n_adcs):
+                self.lib.get_bit_depth(i, ctypes.byref(tmp))
+                bit_depth.append(tmp.value)
 
-        em_gain = [True, False] # Maps gain_type to EM gain enabled
+            em_gain = [True, False] # Maps gain_type to EM gain enabled
 
-        # Populate the list of horizontal shift parameters
-        self.horizontal_shift_parameters = []
-        self._horiz_index = []
-        self._adc_index = []
-        for adc in range(n_adcs):
-            for gain_type in range(2):
-                self.lib.get_number_hs_speeds(adc, gain_type, ctypes.byref(tmp))
-                n_speeds = tmp.value
-                for i in range(n_speeds):
-                    val = c_float()
-                    self.lib.get_hs_speed(adc, gain_type, i, ctypes.byref(val))
-                    self.horizontal_shift_parameters.append( 
-                            (val.value, em_gain[gain_type], bit_depth[adc]) )
-                    self._horiz_index.append(i)
-                    self._adc_index.append(adc)
+            # Populate the list of horizontal shift parameters
+            self.horizontal_shift_parameters = []
+            self._horiz_index = []
+            self._adc_index = []
+            for adc in range(n_adcs):
+                for gain_type in range(2):
+                    self.lib.get_number_hs_speeds(adc, gain_type, ctypes.byref(tmp))
+                    n_speeds = tmp.value
+                    for i in range(n_speeds):
+                        val = c_float()
+                        self.lib.get_hs_speed(adc, gain_type, i, ctypes.byref(val))
+                        self.horizontal_shift_parameters.append( 
+                                (val.value, em_gain[gain_type], bit_depth[adc]) )
+                        self._horiz_index.append(i)
+                        self._adc_index.append(adc)
 
     def get_horizontal_shift_parameters(self):
         return self.horizontal_shift_parameters
@@ -300,25 +367,30 @@ class AndorEmccd:
             raise ValueError("Invalid combination of horizontal shift parameters")
         gain_type = not em_gain
 
-        self.lib.set_adc_channel(self._adc_index[param_ind])
-        self.lib.set_hs_speed(gain_type, self._horiz_index[param_ind])
-        self.lib.set_output_amplifier(gain_type)
+        with self.lock_camera():
+            self.lib.set_adc_channel(self._adc_index[param_ind])
+            self.lib.set_hs_speed(gain_type, self._horiz_index[param_ind])
+            self.lib.set_output_amplifier(gain_type)
 
         self._em_gain_enabled = em_gain
 
     def set_trigger_mode(self, trig_mode):
         """Set the trigger mode between internal and external"""
-        self.lib.set_trigger_mode(trig_mode)
+        with self.lock_camera():
+            self.lib.set_trigger_mode(int(trig_mode))
 
     def set_temperature(self, temp):
         """Set the temperature setpoint to temp deg C"""
-        self.lib.cooler_on()
-        self.lib.set_temperature(temp)
+        with self.lock_camera():
+            self.lib.cooler_on()
+            self.lib.set_temperature(int(temp))
 
     def get_temperature(self):
         """Returns the current camera temperature in deg C, or None if cooler
         is off"""
-        return self.lib.get_temperature()
+        with self.lock_camera():
+            t = self.lib.get_temperature()
+        return t
 
     def set_image_region(self, hStart, hEnd, vStart, vEnd, hBin=1, vBin=1):
         """Set the CCD region to read out and the horizontal and vertical
@@ -328,24 +400,28 @@ class AndorEmccd:
         self.roiWidth = int((1+hEnd-hStart) / hBin)
         self.roiHeight = int((1+vEnd-vStart) / vBin)
 
-        self.lib.set_read_mode(READMODE_IMAGE)
-        self.lib.set_image(int(hBin), int(vBin),  1+int(hStart),
-                                                  1+int(hEnd),
-                                                  1+int(vStart),
-                                                  1+int(vEnd))
+        with self.lock_camera():
+            self.lib.set_read_mode(READMODE_IMAGE)
+            self.lib.set_image(int(hBin), int(vBin),  1+int(hStart),
+                                                      1+int(hEnd),
+                                                      1+int(vStart),
+                                                      1+int(vEnd))
 
 
     def set_em_gain(self, gain):
         """Set the EM gain multiplication factor"""
-        self.lib.SetEMCCDGain(gain)
+        with self.lock_camera():
+            self.lib.set_em_gain(int(gain))
 
     def set_exposure_time(self, time):
         """Set the CCD exposure time in seconds"""
-        self.lib.set_exposure_time(time)
+        with self.lock_camera():
+            self.lib.set_exposure_time(float(time))
 
     def set_shutter_open(self, shutter_open):
         """Open / close the camera shutter"""
-        self.lib.set_shutter(1, 1 if shutter_open else 2, 0, 0)
+        with self.lock_camera():
+            self.lib.set_shutter(1, 1 if shutter_open else 2, 0, 0)
 
     def get_acquisition_timings(self):
         """Returns the actual timings the camera will use, after quantisation
@@ -355,10 +431,11 @@ class AndorEmccd:
         exposure = c_float()
         accumulate = c_float()
         kinetic = c_float()
-        ret = self.lib.get_acquisition_timings( ctypes.byref(exposure),
-                                              ctypes.byref(accumulate),
-                                              ctypes.byref(kinetic))
-        return (exposure.value, accumulate.value, kinetic.value)
+        with self.lock_camera():
+            ret = self.lib.get_acquisition_timings( ctypes.byref(exposure),
+                                                  ctypes.byref(accumulate),
+                                                  ctypes.byref(kinetic))
+            return (exposure.value, accumulate.value, kinetic.value)
 
     def start_acquisition(self, single=False):
         """Start a single or repeated acquisition. If single=False the
@@ -370,17 +447,20 @@ class AndorEmccd:
         else:
             mode = ACQUISITION_RUN_TILL_ABORT
 
-        self.lib.set_acquisition_mode(mode)
-        self.lib.start_acquisition()
+        with self.lock_camera():
+            self.lib.set_acquisition_mode(mode)
+            self.lib.start_acquisition()
 
 
     def stop_acquisition(self):
         """Stop a repeated acquisition"""
-        self.lib.abort_acquisition()
+        with self.lock_camera():
+            self.lib.abort_acquisition()
 
     def wait_for_acquisition(self):
         """Wait for a new image to become available"""
-        self.lib.wait_for_acquisition()
+        with self.lock_camera():
+            self.lib.wait_for_acquisition()
 
 
     def _get_all_images(self):
@@ -388,7 +468,8 @@ class AndorEmccd:
         arrays, or None if no new images"""
         first = c_long()
         last = c_long()
-        n_images = self.lib.get_number_new_images(first, last)
+        with self.lock_camera():
+            n_images = self.lib.get_number_new_images(first, last)
         if n_images == 0:
             return None
 
@@ -396,10 +477,11 @@ class AndorEmccd:
         buf = (c_int * im_size * n_images)()
         valid_first = c_long()
         valid_last = c_long()
-        self.lib.get_images(first, last,
-                            ctypes.cast(buf, POINTER(c_int)),
-                            c_ulong(im_size * n_images),
-                            ctypes.byref(valid_first), ctypes.byref(valid_last))
+        with self.lock_camera():
+            self.lib.get_images(first, last,
+                                ctypes.cast(buf, POINTER(c_int)),
+                                c_ulong(im_size * n_images),
+                                ctypes.byref(valid_first), ctypes.byref(valid_last))
 
 
         assert(first.value == valid_first.value)
@@ -428,12 +510,15 @@ class AndorEmccd:
 
     def _acquisition_thread(self):
         while True:
-            # The GIL is released in the ctypes library call, so we get true
-            # multithreading until wait_for_acquisition() returns
-            self.wait_for_acquisition()
+            # sleep() release the GIL, hence we have true multi-threading here
+            time.sleep(20e-3)
+            if self._stopping.is_set():
+                break
+
             ims = self._get_all_images()
             if ims is None:
                 continue
+
             for im in ims:
                 self.frame_buffer.append(im)
                 for f in self._frame_call_list:
